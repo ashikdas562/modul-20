2c2c3e4c1d39f2da76da1e133cce4d2b2304ea16
unknown <ashikdas652.com>
2021-08-11 02:55:53 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,13 @@
+// const myNumber = -5;
+//  const output = Math.abs(myNumber);
+const myNumber = 2.6398;
+
+// const output = Math.random() * 10;
+// const rounded = Math.floor(output);
+// console.log(rounded);
+
+for (let i =0; i <=30; i++){
+    const output = Math.random() * 6;
+    const rounded = Math.floor(output);
+    console.log(rounded);
+}
