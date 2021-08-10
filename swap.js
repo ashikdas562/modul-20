a8999e35536007e5b66fcc16f2f8c98377b2508a
unknown <ashikdas652.com>
2021-08-11 03:19:37 +0600

    commit

---
+++ swap.js
@@ -0,0 +1,13 @@
+var first = 5;
+var second = 7;
+console.log(first, second);
+
+// first aproach 
+var temp = first;
+first = second;
+second = temp;
+console.log(first, second)
+
+// destructuring 
+[first, second] = [second, first];
+console.log(first, second);
